ca9ee4364a1fce9023424264dded636af6b00609
Antreas <katsoudas_antreas@hotmail.com>
2022-06-09 23:02:20 +0300

    Life removal added

--- replay-starter-js-master/src/index.js
+++ replay-starter-js-master/src/index.js
@@ -5,13 +5,13 @@ import { PlayerLife } from "./playerLife";
 import { Enemy, enemyHeight, enemyWidth} from "./enemy";
 import { EnergyWave, energyHeight, energyWidth } from "./energyWave";
 
+let musicStarted = false;
 let enemiesCount = 0;
-let isMusicPlaying = false;
 const audioArray = ["dbz-1.mp3", "naruto-kokuten.mp3"];
 let audioPlayingCounter=0;
-let isWaveActive = false;
 let wavesCount = 0;
-
+let muted = false;
+let playerLifesCounter = 4;
 export const options = {
   dimensions: "scale-up",
 };
@@ -39,11 +39,33 @@ export const gameProps = {
 const initialState = {
   enemies: [],
   energyWaves: [],
-  // kamehameha: {
-  //   id: "Kamehameha",
-  //   x: -200,
-  //   y: 0,
-  // },
+  playerLifes: 
+  [
+    {
+      id: 1,
+      x: -350,
+      y: 0,
+      lifeHit: false,
+    },
+    {
+      id: 2,
+      x: -350,
+      y: -50,
+      lifeHit: false,
+    },
+    {
+      id: 3,
+      x: -350,
+      y: -100,
+      lifeHit: false,
+    },
+    {
+      id: 4,
+      x: -350,
+      y: -150,
+      lifeHit: false,
+    },
+  ],
   player1: {
     id: "Player1",
     x: -250,
@@ -57,8 +79,8 @@ const initialState = {
 export const Game = makeSprite({
   init({ updateState, preloadFiles }) {
     preloadFiles({
-      audioFileNames: ["boop.wav", "dbz-1.mp3"],
-      imageFileNames: ["goku.png", "playerLife.png", "Namek.png", "icon.png", "kamehameha.png   "],
+      audioFileNames: ["boop.wav", "dbz-1.mp3", "naruto-kokuten.mp3"],
+      imageFileNames: ["goku.png", "playerLife.png", "Namek.png", "icon.png", "kamehameha.png", "playerLife2.png"],
     }).then(() => {
       updateState((state) => ({ ...state, loaded: true }));
     });
@@ -66,19 +88,17 @@ export const Game = makeSprite({
   },
   //This is the loop in which the game is played. It refreshes at 60 frames per second
   loop({ state, device, getInputs }) {
-    if (!state.loaded) return state;
-    if(device.audio(audioArray[audioPlayingCounter]).getStatus()!=="playing"){
-      device.audio(audioArray[audioPlayingCounter]).play();
-    }//else if(device.audio(audioArray[audioPlayingCounter]).getPosition() === device.audio(audioArray[audioPlayingCounter]).getDuration()){
-    //   if(audioPlayingCounter < audioArray.length){
-    //     audioPlayingCounter++;
-    //   }
-    
     let i= 0;
     let enemy,energyWave;
     const inputs = getInputs(); //This reads the inputs of the keyboard and the mouse
-    let { player1, enemies, energyWaves } = state; //These are the states used to update the position of your player objects (You can see how it's used at the render() function)
+    let { player1, enemies, energyWaves, playerLifes, isGameOver } = state; //These are the states used to update the position of your player objects (You can see how it's used at the render() function)
     //inputs.keysDown[] is taking a String to identify the key pressed and recognizes that they key is pressed until it's released
+    
+    if (!state.loaded) return state;
+    if (isGameOver) return;
+    
+    handleMusic(device,inputs);
+    
 
     enemy = spawnEnemy(enemies);
     if (enemy != undefined)
@@ -98,7 +118,7 @@ export const Game = makeSprite({
       (enemy) => enemy.targetHit === false
       )
       .filter(
-        (enemy) => enemy.x > -210
+        (enemy) => !enemy.enemyHitWall
       ),
     ];
     if (inputs.keysJustPressed[" "]){
@@ -111,6 +131,7 @@ export const Game = makeSprite({
     if(energyWaves.length>0){
       energyWaves[0].x += 5;
     }
+    enemyHitWall(enemies, playerLifes);
     didWaveHitTarget(energyWaves,enemies);
 
     if(energyWaves.length > 0){
@@ -119,6 +140,15 @@ export const Game = makeSprite({
         )
         .filter(
           (energy) => energy.x < 400
+        ),
+      ];
+    }
+    if(playerLifesCounter >= 0){
+      if(playerLifesCounter === 0){
+        //isGameOver = true;
+      }
+      playerLifes = [...playerLifes.filter(
+        (life) => life.lifeHit === false
         ),
       ];
     }
@@ -139,6 +169,8 @@ export const Game = makeSprite({
       loaded: true,
       player1,
       energyWaves,
+      playerLifes,
+      isGameOver,
       enemies
     };
   },
@@ -162,26 +194,9 @@ export const Game = makeSprite({
         x: state.player1.x,
         y: state.player1.y,
       }),
-      PlayerLife({
-        id: "life1",
-        x: -350,
-        y: +50,
-      }),
-      PlayerLife({
-        id: "life2",
-        x: -350,
-        y: 0,
-      }),
-      PlayerLife({
-        id: "life3",
-        x: -350,
-        y: -50,
-      }),
-      PlayerLife({
-        id: "life4",
-        x: -350,
-        y: -100,
-      }),
+      ...state.playerLifes.map(({ x, y, id }) =>
+        PlayerLife({ x, y, id: "life" + id })
+      ),
       ...state.enemies.map(({ x, y, id }) =>
         Enemy({ x, y, id: "enemy" + id })
       ),
@@ -191,17 +206,53 @@ export const Game = makeSprite({
       t.rectangle({
         height: device.size.height,
         x: -210,
-        width: 5,
+        width: 2,
         color: "black",
       }),
-      // EnergyWave({
-      //   id: state.kamehameha.id,
-      //   x: state.kamehameha.x,
-      //   y: state.kamehameha.y,
-      // }),
     ];
   },
 });
+
+function enemyHitWall(enemies, playerLifes){
+  let i;
+  for(i = 0; i < enemies.length; i++){
+    if(enemies[i].x === -202){
+      enemies[i].enemyHitWall = true;
+      if(playerLifesCounter > 0){
+        playerLifes[playerLifesCounter-1].lifeHit = true;
+        playerLifesCounter--;
+      }
+    }
+  }
+}
+
+function handleMusic(device, inputs){
+  if(device.audio(audioArray[audioPlayingCounter]).getStatus()!=="playing" && !musicStarted){
+    device.audio(audioArray[audioPlayingCounter]).play();
+    musicStarted = true;
+  }/*else if(device.audio(audioArray[audioPlayingCounter]).getPosition() === device.audio(audioArray[audioPlayingCounter]).getDuration()){
+    if(audioPlayingCounter < audioArray.length){
+      audioPlayingCounter++;
+    }
+  }*/
+  if(inputs.keysJustPressed["+"]){
+    device.audio(audioArray[audioPlayingCounter]).pause();
+    audioPlayingCounter++;
+    device.audio(audioArray[audioPlayingCounter]).play();
+  }
+  if(inputs.keysJustPressed["-"]){
+    device.audio(audioArray[audioPlayingCounter]).pause();
+    audioPlayingCounter--;
+    device.audio(audioArray[audioPlayingCounter]).play();
+  }
+  if(!muted && inputs.keysJustPressed["m"]){
+    device.audio(audioArray[audioPlayingCounter]).pause();
+    muted = true;
+  }else if(muted && inputs.keysJustPressed["m"]){
+    device.audio(audioArray[audioPlayingCounter]).play();
+    muted = false;
+  }
+}
 
 //This function is not used. I am experimenting to create objects dynamically (See replay-bird example from tutorial)
 function newPlayer(device){
@@ -225,7 +276,7 @@ function didWaveHitTarget(energyWaves, enemies){
     for(i = 0; i < enemies.length; i++){
       let enemyTop = enemies[i].y + enemyHeight/2;
       let enemyBottom = enemies[i].y - enemyHeight/2;
-      if(energyX > enemies[i].x && ((energyBottom < enemyTop && energyBottom > enemyBottom ) || (energyTop > enemyBottom && energyTop < enemyBottom))){ //&& energyY > enemyBottom && energyY < enemyTop){
+      if(energyX > enemies[i].x && ((energyBottom < enemyTop && energyBottom > enemyBottom-10 ) || (energyTop > enemyBottom && energyTop < enemyTop))){ //&& energyY > enemyBottom && energyY < enemyTop){
         energyWaves[0].targetHit = true;
         enemies[i].targetHit = true;
       }
@@ -237,22 +288,22 @@ function didWaveHitTarget(energyWaves, enemies){
 function createEnergyWave(player1,energyWaves){
   wavesCount++;
   if(energyWaves.length === 0)
-    return {x: -190, y: player1.y, id: wavesCount-1, targetHit: false};
+    return {x: -190, y: player1.y, id: wavesCount-1, targetHit: false, enemyHitWall: false};
 }
 
 function spawnEnemy(enemies) {
     const enemiesLength = enemies.length;
     enemiesCount++;
     if (enemies.length == 0) { // add first enemy
-      return {x: 400, y: 0, id: enemiesCount-1, targetHit: false};
+      return {x: 400, y: 0, id: enemiesCount-1, targetHit: false, enemyHitWall: false};
     }
     let lastY = enemies[enemies.length-1].y;
     if(enemiesLength > 0 && enemies[enemiesLength-1].x < 250){
       if (enemiesCount % 2 != 0) { // lower 
         console.log("lower");
-        return {x: 400, y: Math.random() * 150 * -1, id: enemiesCount-1, targetHit: false};
+        return {x: 400, y: Math.random() * 150 * -1, id: enemiesCount-1, targetHit: false, enemyHitWall: false};
       } else {// upper
-        return {x: 400, y: Math.random() * 140, id: enemiesCount-1, targetHit: false};
+        return {x: 400, y: Math.random() * 140, id: enemiesCount-1, targetHit: false, enemyHitWall: false};
       }
     }
   
